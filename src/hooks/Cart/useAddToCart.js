a52ecd8fd69dea0609c7d18fc97a7d365b392e62
RyanPrabowo <ryan.prabowo@omniretail.co>
2022-11-15 16:13:10 +0700

    finshing notification using redux

--- src/hooks/Cart/useAddToCart.js
+++ src/hooks/Cart/useAddToCart.js
@@ -1,4 +1,7 @@
+import useNotification from "../useNotification";
+
 const useAddToCart = () => {
+  const { showNotification } = useNotification();
   const addToCartHandler = async (payload) => {
     try {
       const endpoint = `${process.env.REACT_APP_BACKEND_URL}carts/add`;
@@ -8,7 +11,12 @@ const useAddToCart = () => {
         body: JSON.stringify(payload)
       })
       .then(res => res.json())
-      console.log(response);
+
+      showNotification({
+        status: 'success',
+        title: 'Add To Cart',
+        message: 'Adding product to cart successfully!'
+      });
     } catch (error) {
       if (process.env.NODE_ENV === 'development') {
         console.error(error);
