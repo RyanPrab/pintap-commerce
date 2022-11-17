38648d6fe866f51099c6037f1229a1d94749eedd
RyanPrabowo <ryan.prabowo@omniretail.co>
2022-11-17 14:45:42 +0700

    create lazy loading for each pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, Suspense } from 'react';
 import { notificationAction } from './store/notification-slice';
 
 import Header from './components/Header';
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Cart from './pages/Cart';
 import Notification from './components/Notification';
-import NotFound from './pages/NotFound';
+import Spinner from './components/Spinner';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Product = React.lazy(() => import('./pages/Product'));
+const Cart = React.lazy(() => import('./pages/Cart'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   const notificationState = useSelector(state => state.notification);
@@ -41,12 +43,20 @@ function App() {
       }
       <Header/>
       <main className='container mx-auto mt-4 mb-10'>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/product/:productId" element={<Product/>}/>
-        <Route exact path="/cart/:cartId" element={<Cart/>}/>
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
+        <Suspense
+          fallback={
+            <div className='flex w-full justify-center'>
+              <Spinner/>
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/product/:productId" element={<Product/>}/>
+            <Route exact path="/cart/:cartId" element={<Cart/>}/>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
